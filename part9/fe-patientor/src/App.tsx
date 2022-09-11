a6016c4e15fa33a2516f6d196f6d5088b6496045
HuyTranXuan <seuljanvier@gmail.com>
2022-09-12 02:59:25 +0300

    9.18

--- part9/fe-patientor/src/App.tsx
+++ part9/fe-patientor/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import { Button, Divider, Container } from '@material-ui/core';
 
 import { apiBaseUrl } from './constants';
-import { useStateValue } from './state';
+import { Action, useStateValue } from './state';
 import { Patient } from './types';
 
 import PatientListPage from './PatientListPage';
@@ -13,6 +13,12 @@ import { Typography } from '@material-ui/core';
 
 const App = () => {
   const [, dispatch] = useStateValue();
+
+  const setPatientList = (patientListFromApi: Patient[]): Action => ({
+    type: 'SET_PATIENT_LIST',
+    payload: patientListFromApi,
+  });
+
   React.useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
 
@@ -21,7 +27,7 @@ const App = () => {
         const { data: patientListFromApi } = await axios.get<Patient[]>(
           `${apiBaseUrl}/patients`
         );
-        dispatch({ type: 'SET_PATIENT_LIST', payload: patientListFromApi });
+        dispatch(setPatientList(patientListFromApi));
       } catch (e) {
         console.error(e);
       }
