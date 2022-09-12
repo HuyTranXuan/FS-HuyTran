7c18f08031b005dab1f8f79a6b3c0b5b3115f5e6
Huy lap2 <seuljanvier@gmail.com>
2022-09-12 18:23:27 +0300

    9.23

--- part9/be-patientor/src/routes/patients.ts
+++ part9/be-patientor/src/routes/patients.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import patientsServies from '../services/patientsService';
 import toNewPatient from '../utils';
+import { Entry } from '../types';
 
 const router = express.Router();
 
@@ -8,7 +9,6 @@ const router = express.Router();
 router.get('/', (_req, res) => {
   res.send(patientsServies.getPatients());
 });
-
 router.post('/', (req, res) => {
   try {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
@@ -34,6 +34,74 @@ router.get('/:id', (req, res) => {
     res.send(patient);
   } else {
     res.sendStatus(404);
+  }
+});
+router.post('/:id/entries', (req, res) => {
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  try {
+    const entry: Entry = req.body as Entry;
+    // let entryInfo = {
+    //   id: entry.id,
+    //   description: entry.description,
+    //   date: entry.date,
+    //   specialist: entry.specialist,
+    //   diagnosisCodes: entry.diagnosisCodes,
+    // };
+    let entryInfo = { ...entry };
+
+    switch (entry.type) {
+      case 'Hospital':
+        entryInfo = {
+          type: 'Hospital',
+          id: entry.id,
+          description: entry.description,
+          date: entry.date,
+          specialist: entry.specialist,
+          diagnosisCodes: entry.diagnosisCodes,
+          discharge: entry.discharge,
+        };
+        break;
+      case 'HealthCheck':
+        entryInfo = {
+          type: 'HealthCheck',
+          id: entry.id,
+          description: entry.description,
+          date: entry.date,
+          specialist: entry.specialist,
+          diagnosisCodes: entry.diagnosisCodes,
+          healthCheckRating: entry.healthCheckRating,
+        };
+        break;
+      case 'OccupationalHealthcare':
+        entryInfo = {
+          type: 'OccupationalHealthcare',
+          id: entry.id,
+          description: entry.description,
+          date: entry.date,
+          specialist: entry.specialist,
+          diagnosisCodes: entry.diagnosisCodes,
+          employerName: entry.employerName,
+          sickLeave: entry.sickLeave,
+        };
+        break;
+      default:
+        assertNever(entry);
+        break;
+    }
+
+    const addedEntry = patientsServies.addEntry(entryInfo, req.params.id);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
   }
 });
 
--- part9/be-patientor/src/services/patientsService.ts
+++ part9/be-patientor/src/services/patientsService.ts
@@ -1,21 +1,10 @@
 import patients from '../../data/patients';
-import { Patient, PublicPatient } from '../types';
+import { Entry, Patient, PublicPatient } from '../types';
 import { v1 as uuid } from 'uuid';
 
 const getPatients = (): PublicPatient[] => {
   return patients;
 };
-
-// const getNonDescriptiveCardData = (): NonDescriptiveCardData[] => {
-//   return diagnoses.map(({ id, name, rarity, cardType, effect }) => ({
-//     id,
-//     name,
-//     rarity,
-//     cardType,
-//     effect,
-//   }));
-// };
-
 const addPatient = (patientInfo: Patient): Patient => {
   const patient = {
     ...patientInfo,
@@ -25,15 +14,25 @@ const addPatient = (patientInfo: Patient): Patient => {
   patients.push(patient);
   return patient;
 };
-
 const findById = (id: string): PublicPatient | undefined => {
   const patient = patients.find((patient) => patient.id === id);
   return patient;
+};
+const addEntry = (entryInfo: Entry, patientId: string): Entry => {
+  const entry = {
+    ...entryInfo,
+  };
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  entry.id = uuid();
+  patients.find((p) => p.id === patientId)?.entries.concat(entry);
+  return entry;
+  // patients.push(patient);
+  // return patient;
 };
 
 export default {
   getPatients,
   addPatient,
-  // getNonDescriptiveCardData,
   findById,
+  addEntry,
 };
