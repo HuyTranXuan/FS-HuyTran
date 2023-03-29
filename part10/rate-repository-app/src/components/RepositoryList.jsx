189778077071f6987cb020302f28226c6a25b18e
HuyTranXuan <seuljanvier@gmail.com>
2023-03-29 21:08:32 +0300

    set up 10.23

--- part10/rate-repository-app/src/components/RepositoryList.jsx
+++ part10/rate-repository-app/src/components/RepositoryList.jsx
@@ -1,18 +1,18 @@
-import { FlatList, View, StyleSheet } from 'react-native'
+import { FlatList, View, StyleSheet } from 'react-native';
 
-import RepositoryItem from './RepositoryItem'
-import useRepositories from '../hooks/useRepositories'
+import RepositoryItem from './RepositoryItem';
+import useRepositories from '../hooks/useRepositories';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
-})
+});
 
 export const RepositoryListContainer = ({ repositories }) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
-    : []
+    : [];
 
   return (
     <FlatList
@@ -21,15 +21,16 @@ export const RepositoryListContainer = ({ repositories }) => {
       renderItem={({ item }) => <RepositoryItem repository={item} />}
       ItemSeparatorComponent={ItemSeparator}
     />
-  )
-}
+  );
+};
 
-const ItemSeparator = () => <View style={styles.separator} />
+const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories()
+const RepositoryList = ({ searchBy }) => {
+  console.log(searchBy);
+  const { repositories } = useRepositories();
 
-  return <RepositoryListContainer repositories={repositories} />
-}
+  return <RepositoryListContainer repositories={repositories} />;
+};
 
-export default RepositoryList
+export default RepositoryList;
